be20d562775eabbd3d12ee9400b5a058e22e1813
tommy1749 <2321249h@stu.kobe-u.ac.jp>
2024-10-22 11:34:13 +0900

    心臓

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,6 +1,6 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
-let count;
-let cycle;
+let size = 50;
+let increment = 1;
 
 function setup(){
   createCanvas(200, 200);
@@ -12,5 +12,16 @@ function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
   // BLANK[1]
-  ellipse(width / 2, height / 2, size);
-}
+if (keyIsPressed) {
+  increment = 2;
+} else {
+ increment = 1;
+  }
+  if (count < cycle/2){
+ size = count + 50;
+ } else {
+ size = (cycle - count) + 50;
+ }
+   ellipse(width / 2, height / 2, size);
+ }
+           
